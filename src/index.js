7d05787d741539c35f1431aa36d019e375342639
Sohan Manju <sohansonu.m@gmail.com>
2021-04-26 13:17:09 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -121,10 +121,6 @@ class Game extends React.Component {
   }
 }
 
-// ========================================
-
-ReactDOM.render(<Game />, document.getElementById("root"));
-
 function calculateWinner(squares) {
   const lines = [
     [0, 1, 2],
